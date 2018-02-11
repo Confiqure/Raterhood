989581ef15aaa10b8ed75bb73cae070a7f02ffd1
colinrsmall <colinrsmall@yahoo.com>
2018-02-11 09:43:03 -0500

    add zipcode log in getlanglong

--- js/api/apis.js
+++ js/api/apis.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -28,6 +28,7 @@ function getLatLongFromZip(zipcode){
             }
         };
         request.onerror = function() {
+          console.log("zipcode: " + zipcode);
             reject(Error("Network Error"));
         };
         request.send();
@@ -174,4 +175,3 @@ function getZipCode(lat,long)
     request.send();
   });
 }
-
