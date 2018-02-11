18d6dba840a662d6de92fbfd2ddc9bcdd3f4270c
francescomikulis <francescomikulis@gmail.com>
2018-02-11 01:08:39 -0500

    Francesco - Other API Zip code test v3

--- js/api/crime.js
+++ js/api/crime.js
@@ -1,14 +1,27 @@
-function getNearZipCodes(local_zip)
+function getNearbyZipCodes(local_zip)
 {
-    var request = new XMLHttpRequest();
-    request.open( 'GET', 'https://www.zipcodeapi.com/rest/js-JjMMmEtIsTuB9auc1hQ8PimXXHZFrAr724TyHJnxQh9zKeQfV37gkb0fNoUbpPPv/radius.csv/'+local_zip+'/10/miles?minimal');
-    //request.setRequestHeader('Accept', 'application/json');
-    request.onload = function(){
-        console.log( request.response );
-        console.log( request.status );
-        var important_zips = request.response.trim().split("\n");
-        important_zips.shift();
-        console.log( important_zips );
+    return new Zips(function(resolve, reject) {
+    var req = new XMLHttpRequest();
+    req.open( 'GET', 'https://www.zipcodeapi.com/rest/js-JjMMmEtIsTuB9auc1hQ8PimXXHZFrAr724TyHJnxQh9zKeQfV37gkb0fNoUbpPPv/radius.csv/'+local_zip+'/10/miles?minimal');
+    
+    req.onload = function(){
+        if (req.status == 200) {
+            // Resolve the promise with the response text
+            var zips = req.response.trim().split("\n");
+            zips.shift();
+            resolve(zips);
+        }
+        else {
+            // Otherwise reject with the status text
+            // which will hopefully be a meaningful error
+            reject(Error(req.statusText));
+        }
     };
-    request.send();
+    
+    req.onerror = function() {
+        reject(Error("Network Error"));
+    };
+    
+    req.send();
+  });
 }
