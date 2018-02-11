6417b2cd2f8d178b15535c2b97855a82e2eb3d30
francescomikulis <francescomikulis@gmail.com>
2018-02-11 00:41:57 -0500

    Francesco - Testing ZIP data retrieval

--- js/api/crime.js
+++ js/api/crime.js
@@ -1,6 +1,13 @@
-
-
-$(document).ready(function()
+function getNearZipCodes(local_zip)
 {
-    document.getElementById("spotCrimeData").value = getSpotCrimeData();
-});
+    var request = new XMLHttpRequest();
+    request.open( 'GET', 'https://www.zipcodeapi.com/rest/js-JjMMmEtIsTuB9auc1hQ8PimXXHZFrAr724TyHJnxQh9zKeQfV37gkb0fNoUbpPPv/radius.csv/'+local_zip+'/10/miles?minimal');
+    //request.setRequestHeader('Accept', 'application/json');
+    request.onload = function(){
+        console.log( request.response );
+        console.log( request.status );
+        var json_data = JSON.parse( request.response );
+        console.log( json_data['zip_codes'] );
+    };
+    request.send();
+}
